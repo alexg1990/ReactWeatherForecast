08a2720a61f3cf0bd0c3fa3a18a9d573418be0d4
Alex <alexg1990@users.noreply.github.com>
2019-11-10 19:43:03 +0100

    added weather average

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {test} from './weather.js';
-import Forecast from './forecast.js';
+import Forecast from './Forecast.js';
 //const APIcall = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=b633dd9c3c66d5c50f0fccd7c33cd2e7&units=metric";
 console.log(test);
 
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: []
+      weather: [],
+      weatherAverage: []
     };
   }
   componentDidMount() {
@@ -26,31 +27,42 @@ class App extends React.Component {
         temp_max: forecast.main.temp_max,
         temp_min: forecast.main.temp_min
       })})
-      .reduce((prev, curr) => {
-      prev[curr.day] = [...prev[curr.day] || [], curr];
-      return prev;
-      },[])
     }, () => {
+      let weather = this.state.weather.reduce((prev, curr) => {
+        prev[curr.day] = [...prev[curr.day] || [], curr];
+        return prev;
+        },[]);
       let arr = [];
-      for (let key in this.state.weather) {
-        arr = [...arr, this.state.weather[key].reduce((prev,curr) => {
+      for (let key in weather) {
+        arr = [...arr, weather[key].reduce((prev,curr) => {
           prev.temp_max += curr.temp_max;
           prev.temp_min += curr.temp_min;
           prev.icon.push(curr.icon);
+          prev.itemCount++;
           return prev;
         },{
           day: key,
           icon: [],
           temp_max: 0,
-          temp_min: 0
+          temp_min: 0,
+          itemCount: 0
         })];
       }
-      this.setState({weather: arr}, () => console.log(this.state.weather));
+      arr.forEach(day => {
+        day.temp_max = Math.floor(day.temp_max / day.itemCount);
+        day.temp_min = Math.floor(day.temp_min / day.itemCount);
+        function findMostFrequent (re) {
+          return re.sort((a,b) => {
+            return re.filter(v => v===a).length - re.filter(v => v===b).length;
+          }).pop();
+        }
+        day.icon = findMostFrequent(day.icon);
+      });
+      this.setState({weatherAverage: arr}, () => console.log(this.state.weatherAverage));
     });
   }
   render() {
-
-    let weather = this.state.weather.map(item => <Forecast txt={item.txt} icon={item.icon} temp_max={item.temp_max} temp_min={item.temp_min}></Forecast>)
+    let weather = this.state.weatherAverage.map((item,index) => <Forecast key={index} day={item.day} icon={"http://openweathermap.org/img/wn/" + item.icon  + "@2x.png"} temp_max={item.temp_max} temp_min={item.temp_min}></Forecast>)
     return (
       <div style={{display:"flex",flexWrap:"wrap"}}>{weather}</div>
     );
--- src/forecast.js
+++ src/forecast.js
@@ -7,9 +7,9 @@ class Forecast extends React.Component {
   render() {
     return (
       <div style={{padding:10}}>
-        <p style={{textAlign:"center"}}>{this.props.txt}</p>
+        <p style={{textAlign:"center"}}>{this.props.day}</p>
         <img src={this.props.icon} key={this.props.icon} style={{marginLeft:"20%"}}></img>
-        <p style={{textAlign:"center"}}>Max: {this.props.temp_max} Min: {this.props.temp_min}</p>
+        <p style={{textAlign:"center"}}>Max: {this.props.temp_max} °C Min: {this.props.temp_min} °C</p>
       </div>
     )
   }
